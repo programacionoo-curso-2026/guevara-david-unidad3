82f024a9b5336e618472828009a32ed78ad4092e
GUEVARA GUEVARA DAVID HERNAN <dhguevara@gmail.com>
2026-08-03 10:43:07 -0500

    taller23: Segunda Iteracion

--- taller23-goroutines/main.go
+++ taller23-goroutines/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	/*"fmt"*/
 	/*"math/rand"*/
+	"fmt"
 	"sync"
 	/*"time"*/)
 
@@ -16,3 +17,30 @@ var (
 	totalUpdates int
 	updateMutex  sync.Mutex
 )
+
+func main() {
+	var wg sync.WaitGroup
+	orders := generateOrders(20)
+	for i := 0; i < 3; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for _, order := range orders {
+				updateOrderStatus(order)
+			}
+		}()
+	}
+	wg.Wait()
+	fmt.Print("Todas las operaciones completadas. Exiting\n")
+
+}
+
+func generateOrders(count int) []*Order {
+	orders := make([]*Order, count)
+	for i := 0; i < count; i++ {
+		orders[i] = &Order{
+			ID: i + 1, Status: "pending",
+		}
+	}
+	return orders
+}
