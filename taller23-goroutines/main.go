bb7b2cd36c9b3cb630a69b4d1b101ae703ab8963
GUEVARA GUEVARA DAVID HERNAN <dhguevara@gmail.com>
2026-08-03 10:59:49 -0500

    taller23: Tercera Iteración

--- taller23-goroutines/main.go
+++ taller23-goroutines/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	/*"fmt"*/
-	/*"math/rand"*/
 	"fmt"
+	"math/rand"
 	"sync"
-	/*"time"*/)
+	"time"
+)
 
 type Order struct {
 	ID     int
@@ -32,6 +32,24 @@ func main() {
 	}
 	wg.Wait()
 	fmt.Print("Todas las operaciones completadas. Exiting\n")
+	fmt.Printf("Total Actualizaciones %d\n", totalUpdates)
+
+}
+
+func updateOrderStatus(order *Order) {
+	order.mu.Lock()
+	time.Sleep(time.Duration(rand.Intn(500)) * time.Microsecond)
+	status := []string{"Procesando", "Despachando", "Entregando"}[rand.Intn(3)]
+	order.Status = status
+	fmt.Printf("Actualizando orden %d con estado: %s\n", order.ID, status)
+	order.mu.Unlock()
+
+	updateMutex.Lock()
+	defer updateMutex.Unlock()
+
+	currentUpdates := totalUpdates
+	time.Sleep(5 * time.Millisecond)
+	totalUpdates = currentUpdates + 1
 
 }
 
